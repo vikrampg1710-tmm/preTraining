4d25476a88bca8a5c6fd7a756add8d540ef9a1fd
Vikram P G <117505871+vikrampg1710-tmm@users.noreply.github.com>
2023-09-12 12:46:18 +0530

    Update GCDnLCD.cs

--- Spark23 Assignments/GCDnLCD.cs
+++ Spark23 Assignments/GCDnLCD.cs
@@ -1,47 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using static System.ConsoleColor;
-using System.Numerics;
 
 namespace Spark;
 
 public class Q4 {
    public static void main () {
-      List<int> TestCases = new () {6, 7, 8, 9, 10, 30, 72, 135, 180, 225 };
+      List<List<int>> test = new () { new () { 5, 10, 15, 21 }, new () { 6, 18, 36, 54, 96 }, new () { 6, 8, 12, 10, 30, 72, 134 }, new () { 2, 3, 5, 7, 11, 13 }, new () { 17, 34, 51, 68, 36 } };
       Console.WriteLine ("\x1B[4m" + "GCD & LCM Generator:-" + "\x1B[0m");
-      Console.Write ("The test cases are: ");
-      foreach (var item in TestCases) WriteInYellow (item + ", ");
-      Console.CursorLeft -= 2;
-      Console.Write ("\nGCD = ");
-      WriteInYellow ($"{GCDof (TestCases)}");
-      Console.Write ("\nLCM = ");
-      WriteInYellow ($"{LCMof (TestCases)}");
-   }
-   public static void GCDnLCM () {
-      Console.WriteLine ("\x1B[4m" + "GCD & LCM Generator:-" + "\x1B[0m");
-      for (; ; ) {
-         Console.Write ("\nEnter the count of no. of inputs: ");
-         if (int.TryParse (Console.ReadLine (), out int n) && n > 0 && n < 10) {
-            List<int> inputs = new ();
-            for (int i = 0; i < n; i++) {
-               Console.Write ($"Enter input-{i + 1} = ");
-               if (int.TryParse (Console.ReadLine (), out int j)) inputs.Add (j);
-               else i--;
-            }
-            Console.Write ("GCD = ");
-            WriteInYellow($"{GCDof (inputs)}");
-            Console.Write ("LCM = ");
-            WriteInYellow ($"{LCMof (inputs)}");
+      for (int i = 0; i < test.Count; i++) {
+         Console.Write ($"\nTest case-{i + 1}: ");
+         var t = test[i];
+         foreach (var item in t)
+            Console.Write ($"{item}  ", Console.ForegroundColor = Cyan);
+         Console.ResetColor ();
+         Console.Write ("\nGCD = "); WriteInYellow ($"{GCDof (t)}");
+         Console.CursorTop -= 1;
+         Console.Write ("\nLCM = "); WriteInYellow ($"{LCMof (t)}");
+      }
+      Console.Write ("\nNow, lets try. Enter the count of no. of inputs (2 - 7): ");
+      if (int.TryParse (Console.ReadLine (), out int n) && n > 1 && n < 8) {
+         List<int> inputs = new ();
+         for (int i = 0; i < n; i++) {
+            Console.Write ($"Enter input-{i + 1} = ");
+            if (int.TryParse (Console.ReadLine (), out int j)) inputs.Add (j);
+            else i--;
          }
-         else if (n < 0 || n > 30) Console.Write ("Incorrect input!");
-         Console.Write ("\nWant to try again? (y/n): ");
-         if (char.ToLower (Console.ReadKey ().KeyChar) != 'y') {
-            Console.WriteLine ("\nProgram terminated due to incorrect input.", Console.ForegroundColor = Red);
-            Console.ResetColor ();
-            break;
-         }
+         Console.Write ("GCD = "); WriteInYellow ($"{GCDof (inputs)}");
+         Console.CursorTop -= 1;
+         Console.Write ("LCM = "); WriteInYellow ($"{LCMof (inputs)}");
+      }
+      else {
+         Console.WriteLine ("\nProgram terminated due to incorrect input.", Console.ForegroundColor = Red);
+         Console.ResetColor ();
       }
    }
 
